2ad32c07d0d3aa7431c073c8276fe7940c28f436
Dean Cook <deancook371@gmail.com>
2021-07-21 09:51:49 -0700

    Set up Cypress testing

---
+++ cypress/integration/reservation-page.js
@@ -0,0 +1,7 @@
+describe('page is loaded', () => {
+  it('should show an empty form');
+
+  it('should show a group of reservation cards');
+
+  it('should allow the user to create a new reservation');
+});
